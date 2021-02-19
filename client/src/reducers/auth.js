bae1a604f899b59878c59f5f3de6733f2f27acd7
Sandeep <sandip89ster@gmail.com>
2021-02-19 23:01:45 +0530

    Adding login and logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,6 +3,9 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 const initialState = {
@@ -15,14 +18,14 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
-    case 'USER_LOADED':
+    case USER_LOADED:
       return {
         ...state,
         isAuthenticated: true,
         loading: false,
         user: payload,
       };
-    case 'AUTH_ERROR':
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
@@ -30,7 +33,8 @@ export default function (state = initialState, action) {
         isAutheticated: false,
         loading: false,
       };
-    case 'REGISTER_SUCCESS':
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -38,7 +42,9 @@ export default function (state = initialState, action) {
         isAutheticated: true,
         loading: false,
       };
-    case 'REGISTER_FAIL':
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
